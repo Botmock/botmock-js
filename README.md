ece90f8cecd7431f0b05cce8ce8cf8f191fdb2de
Zohaib Ahmed <zohaib92@gmail.com>
2018-04-01 22:45:42 -0400

    Initial JS client. Introduces calls to all endpoints specified by the botmock API and some testing.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Botmock JS Client
+=========
+
+A thin client around the botmock API.
+
+## Installation
+
+  `npm install botmock`
+
+## Usage
+
+TODO: Add Usage
+
+## Tests
+
+  `npm test`
+
+## Contributing
+
+👍🎉 First off, thanks for taking the time to contribute! 🎉👍
+
+When contributing to this repository, please first discuss the change you wish to make via issue, email, or any other method with the owners of this repository before making a change.
+
+## Licensing
+
+We adopt ISC.
---
+++ index.js
@@ -0,0 +1,87 @@
+/**
+ * JS Package for Botmock
+ * Author: Botmock Team
+ * ISC Licensed
+ */
+
+'use strict';
+
+var fetch = require('node-fetch');
+
+module.exports = Botmock;
+
+const BOTMOCK_API_URL = "https://app.botmock.com/api";
+
+
+/**
+ * Constructor for Botmock Client
+ * @param {obj} config 
+ */
+function Botmock(config) {
+    // Ensure that the config has the api_token
+    if (!config.api_token) {
+        throw "api_token not provided in config!"
+    }
+
+    this.api_token = config.api_token;
+    this.debug = config.debug;
+}
+
+Botmock.prototype.fetch = function(endpoint) {
+    const headers = {
+        "Authorization": this.api_token
+    };
+
+    const promise = new Promise((resolve, reject) => {
+        fetch(`${BOTMOCK_API_URL}/${endpoint}`, {headers: headers})
+            .then(res => res.json())
+            .then((data) => {
+                this.logger(data);
+                resolve(data);
+            })
+            .catch(err => reject(err));
+    });
+
+    return promise;
+}
+
+Botmock.prototype.logger = function(msg) {
+    if (this.debug) {
+        console.log(msg);
+    }
+}
+
+Botmock.prototype.teams = function(team_id) {
+    if (team_id) {
+        return this.fetch(`teams/${team_id}`);
+    }
+
+    return this.fetch("teams");
+}
+
+Botmock.prototype.projects = function(team_id, project_id) {
+    if (!team_id) {
+        throw new Error("You must specify a team_id");
+    }
+
+    if (project_id) {
+        return this.fetch(`teams/${team_id}/projects/${project_id}`);
+    }
+
+    return this.fetch(`teams/${team_id}/projects`);
+}
+
+Botmock.prototype.boards = function(team_id, project_id, board_id) {
+    if (!team_id) {
+        throw new Error("You must specify a team_id");
+    }
+    if (!project_id) {
+        throw new Error("You must specify a project_id");
+    }
+
+    if (board_id) {
+        return this.fetch(`teams/${team_id}/projects/${project_id}/boards/${board_id}`);
+    }
+
+    return this.fetch(`teams/${team_id}/projects/${project_id}/boards`);
+}
